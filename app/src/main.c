4eea635e1904b780095e752f1563060275b58ee9
Chitraksha13 <chitraksh.kashyap@gmail.com>
2025-11-16 18:21:02 -0500

    motor code

--- app/src/main.c
+++ app/src/main.c
@@ -3,75 +3,72 @@
 // zephyr/device.h is included eventually through kernel.h
 #include <zephyr/drivers/gpio.h>
 
-// This is accessing the DeviceTree node through its label (can see the entire devicetree in generated zephyr.dts file in the build directory)
-#define LED2 DT_ALIAS(led0)
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-//TEST WHITE LED
-#define WHITE_LED DT_ALIAS(whiteled)
+// Stepper Motor aliases (from devicetree aliases)
+#define STEP_A   DT_ALIAS(step)
+#define DIR_A    DT_ALIAS(dir)
+#define ENABLE_A DT_ALIAS(enable)
 
-// 'gpios' refers to the property name within the led0 node, which is itself inside the leds parent node in the devicetree
-// you can see this^^ in code if you search for the 'zephyr/boards/nordic/nrf54l15dk/nrf54l15dk_common.dtsi' file
-static const struct gpio_dt_spec led_2 = GPIO_DT_SPEC_GET(LED2, gpios);
-static const struct gpio_dt_spec white_led = GPIO_DT_SPEC_GET(WHITE_LED, gpios);
+// GPIO device tree specifications
+static const struct gpio_dt_spec stepPin   = GPIO_DT_SPEC_GET(STEP_A, gpios);
+static const struct gpio_dt_spec dirPin    = GPIO_DT_SPEC_GET(DIR_A, gpios);
+static const struct gpio_dt_spec enablePin = GPIO_DT_SPEC_GET(ENABLE_A, gpios);
+
+// Motor + gearbox constants
+#define MOTOR_STEPS_PER_REV 200          // 1.8° stepper → 200 full steps/rev
+#define GEAR_RATIO          20           // 20:1 planetary gearbox
+#define STEPS_PER_REV       MOTOR_STEPS_PER_REV
+#define TOTAL_STEPS         (STEPS_PER_REV * GEAR_RATIO)   // 200 * 20 = 4000
+
+void rotateSteps(int steps, int delay_us)
+{
+    for (int i = 0; i < steps; i++) {
+        gpio_pin_set_dt(&stepPin, 1);
+        k_usleep(delay_us);
+
+        gpio_pin_set_dt(&stepPin, 0);
+        k_usleep(delay_us);
+    }
+}
 
 int main(void)
 {
-    // led_2.port is used because you need to check if the gpio controller itself is ready to be used, if you just do led_2.pin you will get a number like '9'
-    // also check white_led port is ready
-    if (!device_is_ready(led_2.port) || !device_is_ready(white_led.port))
-    {
-        printk("GPIO device not ready\n");
-        return 0;
+    
+    // Configure pins
+    gpio_pin_configure_dt(&stepPin, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&dirPin, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&enablePin, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE);
+
+    printk("Stepper Motor Ready\n");
+
+    while (1) {
+        // Blink LED so you know loop is running
+        gpio_pin_toggle_dt(&led0);
+        gpio_pin_set_dt(&dirPin, 1);
+        rotateSteps(TOTAL_STEPS, 800);
+        k_msleep(500);                      // 2 s pause between revolutions
     }
+    
+    /*
+    printk("GPIO PC14 / PC15 Test Start\n");
+
+    gpio_pin_configure_dt(&stepPin, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&dirPin,  GPIO_OUTPUT_INACTIVE);
 
     int state = 0;
-    int ret;
 
-    // configuration of led_2 pin (on-board LED) - optional
-    ret = gpio_pin_configure_dt(&led_2, GPIO_OUTPUT);
-    if (ret != 0)
-    {
-        printk("Failed to configure led_2: %d\n", ret);
-        return 0;
+    while (1) {
+        state = !state;
+
+        gpio_pin_set_dt(&stepPin, state);
+        gpio_pin_set_dt(&dirPin,  state);
+
+        printk("Toggled: STEP=%d   DIR=%d\n", state, state);
+
+        k_msleep(2000);
     }
-
-    // configuration of white_led pin (external LED)
-    ret = gpio_pin_configure_dt(&white_led, GPIO_OUTPUT);
-    if (ret != 0)
-    {
-        printk("Failed to configure white_led: %d\n", ret);
-        return 0;
-    }
-
-    while (true)
-{
-    state = !state;
-
-    // blink on-board LED
-    ret = gpio_pin_set_dt(&led_2, state);
-    if (ret != 0)
-    {
-        printk("ERROR: failed to set LED2 (on-board)\n");
-        return 0;
-    }
-
-    // blink external LED
-    ret = gpio_pin_set_dt(&white_led, state);
-    if (ret != 0)
-    {
-        printk("ERROR: failed to set WHITE_LED (external)\n");
-        return 0;
-    }
-    printk("----------------------\n");
-    printk("white_led: port=%s pin=%d flags=0x%x\n",
-       white_led.port->name, white_led.pin, white_led.dt_flags);
-
-    printk("LED2 (on-board) state: %d\n", state);
-    printk("WHITE_LED (external) state: %d\n", state);
-
-    k_msleep(4000);
-}
-
-
-    return 0;
+    */
 }
